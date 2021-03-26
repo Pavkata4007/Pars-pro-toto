8c7b68f335f88a06c4b76b939065c0bb5fe524c7
pnyagolov <pnyagolov@htmlbox.net>
2021-03-26 11:00:02 +0200

    client's cahnges.

--- src/assets/js/modules/mobile-nav.js
+++ src/assets/js/modules/mobile-nav.js
@@ -9,9 +9,10 @@ const $body = $('body');
 
 $navigation.on('click', function() {
 	const $this = $(this);
+	const $mobileNav = $this.find('.js-mobile-nav');
 	const $nav = $('.js-nav');
 	const $button = $('.js-button');
 
-	$this.toggleClass('mobile-nav--active');
+	$mobileNav.toggleClass('mobile-nav--active');
 	$nav.toggleClass('is-active');
 });
